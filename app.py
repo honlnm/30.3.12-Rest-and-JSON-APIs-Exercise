1a456c5baf270e8a5533133c46b4970bc2c274fe
honlnm <nicole.m.honl@gmail.com>
2024-01-05 15:14:57 -0600

    testing

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-"""Flask app for Cupcakes"""
+import os
+from flask import Flask, request, jsonify, render_template
+from models import db, connect_db, Cupcake
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('secret_key')
+
+############## CONNECT DB ##############
+
+connect_db(app)
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+
+############## ROUTES ##############
+
+@app.route('/api/cupcakes', methods=["GET", "POST"])
+def list_cupcakes():
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+    return jsonify(cupcakes=serialized)
+
+@app.route('/api/cupcakes/<int:id>')
+def single_cupcake_data(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
--- models.py
+++ models.py
@@ -1,1 +1,30 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Float, Integer, DateTime
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+    __tablename__ = "cupcakes"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String(), nullable=False)
+    size = db.Column(db.String(), nullable=False)
+    rating = db.Column(db.Float(2), nullable=False)
+    image = db.Column(db.String(), nullable=False, default="https://tinyurl.com/demo-cupcake")
+
+    def serialize(self):
+        """Serialize a cupcake SQLAlchemy obj to dictionary"""
+        return {
+            "id":self.id,
+            "flavor":self.flavor,
+            "size":self.size,
+            "rating":self.rating,
+            "image":self.image
+        }
+
+############## CONNECT DB ##############
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
